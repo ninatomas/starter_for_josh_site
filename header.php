d057f08319a4d8cc3096c9ebadb34c5571f09eb6
ninatomas <61775981+ninatomas@users.noreply.github.com>
2020-03-20 06:03:08 +0800

    Update header.php

--- header.php
+++ header.php
@@ -27,7 +27,7 @@
 		         echo ' - page '. $paged; }
 		   ?>
 </title>
-
+<meta name="description" content="<?php bloginfo('description'); ?>">
 
 
     <!-- HTML5 shim and Respond.js for IE8 support of HTML5 elements and media queries -->
