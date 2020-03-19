cde6a5086b47eebfd98b1195a1b0adc5165cfd2f
ninatomas <61775981+ninatomas@users.noreply.github.com>
2020-03-20 06:05:50 +0800

    Update header.php

--- header.php
+++ header.php
@@ -28,6 +28,14 @@
 		   ?>
 </title>
 <meta name="description" content="<?php bloginfo('description'); ?>">
+<!—Viewport responsive  -->
+<meta name="viewport" content="width=device-width, initial-scale=1.0">		
+	
+<link rel="shortcut icon" href="<?php bloginfo('template_directory'); ?>/_/img/favicon.ico">
+	<!-- This is the traditional favicon.
+		 - size: 16x16 or 32x32
+		 - transparency is OK -->
+    <!—Google fonts  -->
 
 
     <!-- HTML5 shim and Respond.js for IE8 support of HTML5 elements and media queries -->
