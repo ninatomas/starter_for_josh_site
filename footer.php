86ac0946425183e52cb9679c4ddebe2159fe06d5
ninatomas <61775981+ninatomas@users.noreply.github.com>
2020-03-19 21:28:05 +0800

    Update footer.php

--- footer.php
+++ footer.php
@@ -3,13 +3,13 @@
         <div class="row">
             <div class="col-md-6 text-center footer-content">
                 <p><?php bloginfo('name'); ?></p>
-                <img src=" http://206.189.45.97/~mesh22/sub/wp-content/themes/starter_for_josh_site/images/game.jpg" alt=“tumblr” >
-                <img src=" http://206.189.45.97/~mesh22/sub/wp-content/themes/starter_for_josh_site/images/game.jpg" alt=“rss” >
+                <img src=" http://206.189.45.97/~mesh1/wp-content/themes/josh_theme_wp_fiinished/images/tumblr.jpg" alt=“tumblr” >
+                <img src=" http://206.189.45.97/~mesh1/wp-content/themes/josh_theme_wp_fiinished/images/tumblr.jpg" alt=“rss” >
             </div>
             <div class="col-md-6 text-center footer-content">
                 <p>Terms and Conditions</p>
-                <img src=" http://206.189.45.97/~mesh22/sub/wp-content/themes/starter_for_josh_site/images/game.jpg" alt=“linkedin” >
-                <img src=" http://206.189.45.97/~mesh22/sub/wp-content/themes/starter_for_josh_site/images/game.jpg" alt=“facebook” >
+                <img src=" http://206.189.45.97/~mesh1/wp-content/themes/josh_theme_wp_fiinished/images/tumblr.jpg" alt=“linkedin” >
+                <img src=" http://206.189.45.97/~mesh1/wp-content/themes/josh_theme_wp_fiinished/images/tumblr.jpg" alt=“facebook” >
             </div>
         </div>
     </div>
