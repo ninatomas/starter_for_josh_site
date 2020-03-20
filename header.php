94f11b75bc0468a46702b95075e0265bceb2991b
ninatomas <61775981+ninatomas@users.noreply.github.com>
2020-03-20 08:17:29 +0800

    Update header.php

--- header.php
+++ header.php
@@ -1,47 +1,36 @@
 <!DOCTYPE html>
 <html lang="en">
+
   <head>
-    <meta charset="<?php bloginfo('charset'); ?>">
+      <meta charset="<?php bloginfo('charset'); ?>"> 
     <meta charset="utf-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <!-- The above 3 meta tags *must* come first in the head; any other head content must come *after* these tags -->
-      
-    <link rel="shortcut icon" href="<?php bloginfo('template_directory'); ?>/_/img/favicon.ico">  
-      
-    <title>   <?php
-		      if (function_exists('is_tag') && is_tag()) {
-		         single_tag_title("Tag Archive for &quot;"); echo '&quot; - '; }
-		      elseif (is_archive()) {
-		         wp_title(''); echo ' Archive - '; }
-		      elseif (is_search()) {
-		         echo 'Search for &quot;'.wp_specialchars($s).'&quot; - '; }
-		      elseif (!(is_404()) && (is_single()) || (is_page())) {
-		         wp_title(''); echo ' - '; }
-		      elseif (is_404()) {
-		         echo 'Not Found - '; }
-		      if (is_home()) {
-		         bloginfo('name'); echo ' - '; bloginfo('description'); }
-		      else {
-		          bloginfo('name'); }
-		      if ($paged>1) {
-		         echo ' - page '. $paged; }
-		   ?>
-</title>
-      
-      
-      
-      
-<meta name="description" content="<?php bloginfo('description'); ?>">
-<!—Viewport responsive  -->
-<meta name="viewport" content="width=device-width, initial-scale=1.0">		
-	
-<link rel="shortcut icon" href="<?php bloginfo('template_directory'); ?>/_/img/favicon.ico">
-	<!-- This is the traditional favicon.
-		 - size: 16x16 or 32x32
-		 - transparency is OK -->
-    <!—Google fonts  -->
 
+      <link rel="shortcut icon" href="<?php bloginfo('template_directory'); ?>/_/img/favicon.ico">
+
+      <title>
+          <?php
+                      if (function_exists('is_tag') && is_tag()) {
+                         single_tag_title("Tag Archive for &quot;"); echo '&quot; - '; }
+                      elseif (is_archive()) {
+                         wp_title(''); echo ' Archive - '; }
+                      elseif (is_search()) {
+                         echo 'Search for &quot;'.wp_specialchars($s).'&quot; - '; }
+                      elseif (!(is_404()) && (is_single()) || (is_page())) {
+                         wp_title(''); echo ' - '; }
+                      elseif (is_404()) {
+                         echo 'Not Found - '; }
+                      if (is_home()) {
+                         bloginfo('name'); echo ' - '; bloginfo('description'); }
+                      else {
+                          bloginfo('name'); }if ($paged>1) {
+                         echo ' - page '. $paged; }
+          ?>
+</title>
+
+    <!-- Bootstrap -->
 
     <!-- HTML5 shim and Respond.js for IE8 support of HTML5 elements and media queries -->
     <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
@@ -49,20 +38,11 @@
       <script src="https://oss.maxcdn.com/html5shiv/3.7.3/html5shiv.min.js"></script>
       <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
     <![endif]-->
-      <link href="https://fonts.googleapis.com/css?family=Raleway:400,800&display=swap" rel="stylesheet">
-      <?php wp_head(); ?> <!—very important that this is added-->
-    </head>
-    
-    
-    
-    
-    
-    
-<body>
-      
-<header class="container-fluid herobg">
-          
-      <nav class="navbar">
+      <link href="https://fonts.googleapis.com/css?family=Raleway:400,800&display=swap" rel="stylesheet"><?php wp_head(); ?>
+      </head>
+  <body>
+         <header class="container-fluid herobg">
+     <nav class="navbar">
   <div class="container-fluid">
     <div class="navbar-header">
       <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#myNavbar">
@@ -70,24 +50,22 @@
         <span class="icon-bar"></span>
         <span class="icon-bar"></span> 
       </button>
-      <a class="navbar-brand" href="#"><span class="josh">JOSH</span><span class="orange">WHITKIN</span></a>
+
+        <a class="navbar-brand" href="#"><span class="josh">JOSH</span><span class="orange">WHITKIN</span></a>
     </div>
     <div class="collapse navbar-collapse" id="myNavbar">
       <ul class="nav navbar-nav navbar-right">
-        <li class="active"><a href="#">PROJECTS</a></li>
+        <li class="active"><a href="#">HOME</a></li>
+        <li><a href="#">PROJECTS</a></li>
         <li><a href="#">PUBLICATIONS</a></li> 
-        <li><a href="#">CV</a></li>
-          <li><a href="#">CONTACT</a></li> 
-          
+        <li><a href="#">CV</a></li> 
+        <li><a href="#">CONTACT</a></li> 
       </ul>
-      
+
     </div>
   </div>
-</nav>
-      
-    
-      <div class="container">
-        <h1> Games Design For The Future</h1>
-        </div>
-          
-</header>
+</nav> 
+              <div class="container">
+              <h1> Games Design for the future</h1>
+          </div>
+      </header>
