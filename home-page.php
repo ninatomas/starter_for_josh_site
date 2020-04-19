c19e52e1939c416ee81f6a9066f0678059238277
ninatomas <61775981+ninatomas@users.noreply.github.com>
2020-04-19 16:39:22 +0800

    Create home-page.php

---
+++ home-page.php
@@ -0,0 +1,74 @@
+<?php get_header(); /* Tells WordPress to include header.php */ ?>
+<?php get_header(); /* Tells WordPress to include header.php */ ?>
+     <section class="container-fluid aboutbg text-center">
+        <div class="container">
+            
+        
+<?php
+global $more;//define a global variable
+$more = 0;// the global varibale is now equal to 0
+query_posts('cat=2');//look for posts that have the category of 2
+if(have_posts()) ://if we have posts to display
+while(have_posts()) :the_post();//LOOP through all the posts and find the one that has a category of 2 get thet title and content
+?>
+<h2 class="animated fadeInLeft"><a href="<?php the_permalink(); ?>"><?php the_title();?></a></h2> 
+            <div><p class="about-text animated fadeInRight"><?php the_content() ?></p></div>
+<?php
+endwhile;
+endif;
+wp_reset_query();?>    
+    	
+ <div class="blocker"></div>
+            
+      </div><!--  container-->
+    </section>
+
+
+      <section class="container-fluid articlebg">
+          <div class="container">
+          <div class="row"> 
+              
+          <div>
+              <h3 class="text-center article-title"> LATEST ARTICLES</h3>
+              </div>
+         
+      <div class="col-md-4 article-cent"> 
+          
+<?php $post_id = 17;
+$queried_post = get_post($post_id);?>
+<?php echo get_the_post_thumbnail($queried_post, 'full', array('class' => 'img-responsive')); ?>
+<h4><?php echo $queried_post->post_title; ?></h4>
+<?php echo $queried_post->post_excerpt; ?>
+<?php echo '<a class="readmore" href="'.get_permalink($queried_post).'"><br>CONTINUE READING</a>';?>                       
+          
+      </div>  
+              
+          <div class="col-md-4 article-cent"> 
+              
+<?php $post_id = 19;
+$queried_post = get_post($post_id);?>
+<?php echo get_the_post_thumbnail($queried_post, 'full', array('class' => 'img-responsive')); ?>
+<h4><?php echo $queried_post->post_title; ?></h4>
+<?php echo $queried_post->post_excerpt; ?>
+<?php echo '<a class="readmore" href="'.get_permalink($queried_post).'"><br>CONTINUE READING</a>';?>
+              
+      </div>
+              
+          <div class="col-md-4 article-cent"> 
+              
+<?php $post_id = 21;
+$queried_post = get_post($post_id);?>
+<?php echo get_the_post_thumbnail($queried_post, 'full', array('class' => 'img-responsive')); ?>
+<h4><?php echo $queried_post->post_title; ?></h4>
+<?php echo $queried_post->post_excerpt; ?>
+<?php echo '<a class="readmore" href="'.get_permalink($queried_post).'"><br>CONTINUE READING</a>';?>
+      
+              </div>
+              
+              
+              
+              
+      </div>
+    </div>
+    </section>
+<?php get_footer(); /* Tells WordPress to include footer.php */ ?>
