f9e1ae332d37ac190eddd562f607c5d4228b1793
ninatomas <61775981+ninatomas@users.noreply.github.com>
2020-04-19 16:39:16 +0800

    Update functions.php

--- functions.php
+++ functions.php
@@ -3,6 +3,7 @@ function theme_add_bootstrap() {
 	wp_enqueue_style( 'bootstrap-css', get_template_directory_uri() . '/css/bootstrap.min.css' );
 wp_enqueue_style( 'style-css', get_template_directory_uri() . '/style.css' );
     wp_enqueue_style( 'test', get_stylesheet_directory_uri() . '/responsive.css' );
+    wp_enqueue_style( 'tester', get_stylesheet_directory_uri() . '/css/animate.min.css' );
 wp_enqueue_script('jquery', 'https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js', array(), null, false);
 wp_enqueue_script( 'bootstrap-js', get_template_directory_uri() . '/js/bootstrap.min.js', array(), '3.0.0', true );
 }
